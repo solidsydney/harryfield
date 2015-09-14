a4a08def90fff1fd8745e7d968dd26d563339d2b
Michael Ocheje <mocheje@c2gconsulting.com>
2015-09-14 13:11:48 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harryschool::Application.config.secret_token = '2bf40ebb20e225c414a1a8c3172eb98261503d89209b1f133382141b0d1c3d3bdf298965d5f860aa642d4f23413f49bbe5eafa6eb69539c03406138d70275819'
